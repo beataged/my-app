b97a4e929e44c238a096c0fe121652455519f4df
beataged <86911933+beataged@users.noreply.github.com>
2022-01-22 17:20:02 +0200

    commit

--- src/App.js
+++ src/App.js
@@ -31,9 +31,13 @@ const expenses = [
     },
   ];
 
+  const addExpenseHandler = expense =>{
+    console.log("in app.js");
+    console.log(expense);
+  }
   return (
     <div>
-    <NewExpense></NewExpense>
+    <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
     <Expenses items={expenses}></Expenses>
     </div>
   );
